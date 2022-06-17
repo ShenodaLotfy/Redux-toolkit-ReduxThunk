0380bef0818efb3b5ed3945c8b4f5cd0a59daa34
Shenoda <shenodalotfy1@gmail.com>
2022-06-17 05:10:20 +0200

    Reduxtoolkit - counterSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,17 @@ import { applyMiddleware, createStore } from "redux";
 import reducer from "../reducers/rootReducer";
 import reduxThunk from "redux-thunk";
 import reducerCombine from "../reducers/reducersCombine";
+import counterSlice from "../counterSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const reducers = reducerCombine;
-const store = createStore(reducers, enhancer(applyMiddleware(reduxThunk)));
+// const store = createStore(reducers, enhancer(applyMiddleware(reduxThunk)));
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice,
+  },
+});
 
 export default store;
